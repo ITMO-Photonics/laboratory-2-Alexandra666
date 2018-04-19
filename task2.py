a5ae9b04241e19a400eebd6e2ca932b9fc633662
WolfhounD <michael@bitbucker.org>
2018-04-19 22:37:34 +0300

    euler

--- task2.py
+++ task2.py
@@ -4,32 +4,37 @@ import matplotlib.animation as animation
 
 g=9.8
 Nsteps=500
-t=np.linspace(0.,5.,Nsteps+1)
+#t=np.linspace(0.,5.,Nsteps+1)
 #y=np.empty_like(t)
 #v=np.empty_like(t)
-h=t[1]-t[0]
-
-r = np.array([5.,50.])
-v = np.array([0.,0.])
+#h=t[1]-t[0]
+h=0.01
+r = np.array([25.,25.])
+v = np.array([-5.,0.])
 
 fig, ax = plt.subplots()
 
 circle, = ax.plot([], [], 'bo', ms=10)
 
 def init():
-    ax.set_xlim([0., 100.])
-    ax.set_ylim([-130., 100.])
+    ax.set_xlim([0., 50.])
+    ax.set_ylim([-50., 50.])
     return circle,
 
 def updatefig(frame):
-    r[1] = r[1]+h*v[1]
-    v[1] = v[1]-g*h    
-    if r[1]<=0:    
+    v[1] = v[1]-g*h
+    r[1] = r[1]+h*v[1]    
+    if r[1]<=0.:    
         v[1]=-v[1]
-        v[1] = v[1]-g*h  
+   
+    v[0] = v[0]-0*h
+    r[0] = r[0]+h*v[0]
+    if r[0]<=0. or r[0]>=50.:
+        v[0]=-v[0]
+
     circle.set_xdata(r[0])
     circle.set_ydata(r[1])
     return circle,
 
-anim = animation.FuncAnimation(fig, updatefig, frames=Nsteps*1000, init_func=init, interval=0.001, blit=True, repeat=False)
+anim = animation.FuncAnimation(fig, updatefig, frames=200000, init_func=init, interval=0.01, blit=True, repeat=False)
 plt.show()
